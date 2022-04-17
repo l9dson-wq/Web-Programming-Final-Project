de38cceefdb1a68abbc7aee976f98dbdfa402e32
Andelson Lopez <69158247+l9dson-wq@users.noreply.github.com>
2022-04-17 12:09:06 -0400

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -30,7 +30,6 @@ export const AuthContextProvider = ({children}) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             // console.log(currentUser);
-            console.log(currentUser);
             setUser(currentUser);
         })
         return () => {
